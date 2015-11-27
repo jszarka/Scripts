64bb2fd2f99e70674517c5a2dca370a61ac0f291
James Szarka <james@backupplan.ca>
2015-11-26 20:37:32 -0700

    robobk_0.4.ps1

---
+++ robobk_0.4.ps1
@@ -0,0 +1,33 @@
+#Network mounter
+$uncServer = "\\192.168.20.35"
+$uncFullPath = "$uncServer\d$"
+$username = "administrator"
+$password = "scada3400"
+ 
+net use $uncServer $password /USER:$username
+
+
+#Data Values
+$SourceFolder = "\\192.168.20.35\d$\ClearSCADA\"
+$DestinationFolder = "D:\T3400-D-Wed"
+$Logfile = "d:\robotest\Robocopy.log"
+$threads = "1"
+$EmailFrom = "events@szarka.ca"
+$EmailTo = "jszarka@szarka.ca"
+$EmailBody = "Robocopy has finished. Please Check Attached Logs for success."
+$EmailSubject = "Glencoe Morningside Robocopy Summary"
+$SMTPServer = "mail.szarka.ca"
+$SMTPPort = "25"
+$Username = "events@szarka.ca"
+$Password = "thunder54"
+# Copy Folder with Robocopy
+Robocopy $SourceFolder $DestinationFolder /MIR /LOG:$Logfile /TEE
+
+# Send E-mail message with log file attachment
+$Message = New-Object Net.Mail.MailMessage($EmailFrom, $EmailTo, $EmailSubject, $EmailBody)
+$Attachment = New-Object Net.Mail.Attachment($Logfile, 'text/plain')
+$Message.Attachments.Add($Attachment)
+$SMTPClient = New-Object Net.Mail.SmtpClient("mail.szarka.ca", 2525)
+$SMTPClient.EnableSsl = $false
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+$SMTPClient.Send($Message)
